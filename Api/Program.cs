e52f6942007429d3aaedc142ec99238bbda535e7
GaboAfk <gabrielmorffe95@gmail.com>
2024-09-23 18:16:40 -0300

    link

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,4 +1,6 @@
 using Api.Extensions;
+using Microsoft.EntityFrameworkCore;
+using Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,11 @@ builder.Services.ConfigureCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
